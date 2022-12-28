499778869b9a0040941727a4fa160b9b312753c2
amarupd <amarduttupadhyay@gmail.com>
2022-12-28 13:06:31 +0530

    scs

--- model/paymentsmodel.js
+++ model/paymentsmodel.js
@@ -1,29 +1,19 @@
 module.exports=(sequelize,DataTypes)=>{
     const Orders=sequelize.define("orders",{
-        orderNumber:{
-            type:DataTypes.NUMBER,
-            allowNull:false
-        },
-        orderDate:{
-            type:DataTypes.DATE,
-            allowNull:false
-        },
-        requiredDate:{
-            type:DataTypes.DATE,
-            allowNull:false
-        },
-        shippedDate:{
-            type:DataTypes.DATE
-        },
-        status:{
-            type:DataTypes.BOOLEAN,
-            allowNull:false
-        },
-        comments:{
-            type:DataTypes.TEXT
-        },
         customerNumber:{
             type:DataTypes.NUMBER,
+            allowNull:false
+        },
+        checkNumber:{
+            type:DataTypes.STRING,
+            allowNull:false
+        },
+        paymentDate:{
+            type:DataTypes.DATE,
+            allowNull:false
+        },
+        amount:{
+            type:DataTypes.FLOAT,
             allowNull:false
         }
     });
