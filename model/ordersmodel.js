d16b379ed0d8420024c8f69be57530c4d7413f54
amarupd <amarduttupadhyay@gmail.com>
2022-12-28 13:00:26 +0530

    com

---
+++ model/ordersmodel.js
@@ -0,0 +1,31 @@
+module.exports=(sequelize,DataTypes)=>{
+    const Orderdetails=sequelize.define("orderdetails",{
+        orderNumber:{
+            type:DataTypes.NUMBER,
+            allowNull:false
+        },
+        productCode:{
+            type:DataTypes.STRING,
+            allowNull:false
+        },
+        quantityOrdered:{
+            type:DataTypes.NUMBER,
+            allowNull:false
+        },
+        priceEach:{
+            type:DataTypes.FLOAT,
+            allowNull:false
+        },
+        orderLineNumber:{
+            type:DataTypes.BOOLEAN
+        },
+        priceEach:{
+            type:DataTypes.FLOAT,
+            allowNull:false
+        },
+        orderLineNumber:{
+            type:DataTypes.BOOLEAN
+        }
+    });
+    return Orderdetails;
+}
