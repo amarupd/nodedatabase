0c6b931f1f1a2dbb6536c41e6b90b1c498601f50
amarupd <amarduttupadhyay@gmail.com>
2022-12-28 13:04:44 +0530

    saa

---
+++ model/paymentsmodel.js
@@ -0,0 +1,31 @@
+module.exports=(sequelize,DataTypes)=>{
+    const Orders=sequelize.define("orders",{
+        orderNumber:{
+            type:DataTypes.NUMBER,
+            allowNull:false
+        },
+        orderDate:{
+            type:DataTypes.DATE,
+            allowNull:false
+        },
+        requiredDate:{
+            type:DataTypes.DATE,
+            allowNull:false
+        },
+        shippedDate:{
+            type:DataTypes.DATE
+        },
+        status:{
+            type:DataTypes.BOOLEAN,
+            allowNull:false
+        },
+        comments:{
+            type:DataTypes.TEXT
+        },
+        customerNumber:{
+            type:DataTypes.NUMBER,
+            allowNull:false
+        }
+    });
+    return Orders;
+}
