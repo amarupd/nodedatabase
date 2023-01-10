8247b8f939a17d788bac3d742df757efec90c3c3
amarupd <amarduttupadhyay@gmail.com>
2023-01-10 13:02:48 +0530

    commit

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ const app = express();
 const redisPort = "redis://127.0.0.1:6379"
 
 const client = redis.createClient(redisPort);
+client.on("error", (err) => {
+    console.log(err);
+})
+
 
 var corOptions = {
     origin: 'https://localhost:8001'
@@ -70,6 +74,34 @@ app.use('/api', prodrouter);
 
 app.get("", (req, res) => {
     res.json({ message: 'hello from api' });
+});
+
+//redis import
+
+app.get("/temp", (req, res) => {
+    const cityVal = req.query.search;
+    try {
+        client.get(cityVal, async (err, jobs) => {
+            if (err) throw err;
+    
+            if (jobs) {
+                res.status(200).send({
+                    jobs: JSON.parse(jobs),
+                    message: "data retrieved from the cache"
+                });
+            }
+            else {
+                const jobs = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityVal},IN&appid=1378804aeafe0b631a88802bfd8d17d6`);
+                client.setex(cityVal, 600, JSON.stringify(jobs.data));
+                res.status(200).send({
+                    jobs: jobs.data,
+                    message: "cache miss"
+                });
+            }
+        });
+    } catch(err) {
+        res.status(500).send({message: err.message});
+    }
 });
 
 //port
