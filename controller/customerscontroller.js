7aa9c04248647cfb085f025d9ad78af53e86244c
amarupd <amarduttupadhyay@gmail.com>
2023-01-14 11:27:19 +0530

    commit

--- controller/customerscontroller.js
+++ controller/customerscontroller.js
@@ -1,4 +1,13 @@
 const db = require("../model");
+const redis = require("redis");
+
+const redisPort = "redis://127.0.0.1:6379"
+// const redisPort = "redis://default:ovDFb4qIVC7PoaIdIDlsaE4ymM97Aaf3@redis-12561.c264.ap-south-1-1.ec2.cloud.redislabs.com:12561"
+const client = redis.createClient(redisPort);
+
+client.on("error", (err) => {
+    console.log(err);
+})
 
 
 //create our main model...
@@ -40,7 +49,37 @@ const getAllCustomer = async (req, res) => {
 }
 
 
+/******************************************************************************************************************************* */
+
+const getOne = async (req, res) => {
+    const id = req.query.id
+    try {
+        client.get(id, async (err, customer) => {
+            if (err) throw err;
+            if (customer) {
+                console.log("catched from redis");
+                res.status(200).send({
+                    customer: JSON.parse(customer),
+                    message: " customer retrieved from the cache"
+                });
+            }
+            else {
+                let customer = await Customer.findOne({ where: { customerNumber: id } })
+                client.setex(id,600,JSON.stringify(customer));
+                res.status(200).send(customer);
+                console.log("fetched from mysql")
+            }
+        })
+    }catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+
+}
+
+/********************************************************************************************************************************* */
+
 module.exports = {
     addCustomer,
-    getAllCustomer
+    getAllCustomer,
+    getOne
 };
